433a24d43e76014fe6ca0dd92a50441c1141977e
Simran Nayak <simrannayak2006@gmail.com>
2025-08-15 22:53:45 +0530

    3

--- src/components/discussions/DiscussionCard.jsx
+++ src/components/discussions/DiscussionCard.jsx
@@ -4,7 +4,7 @@ import { motion } from 'framer-motion';
 import { format } from 'date-fns';
 import { Link } from 'react-router';
 import { CheckBadgeIcon } from '@heroicons/react/24/solid';
-import Tag from './Tag';
+import Tag from './tag';
 import UserAvatar from './UserAvatar';
 
 const DiscussionCard = ({ question }) => {
